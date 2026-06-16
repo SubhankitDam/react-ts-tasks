5a99b627c5bad6d959cd2a031693b3854271cfa6
Mirag09 <damsubhankit@gmail.com>
2026-06-17 01:32:38 +0530

    style: design the interface for adding tasks and displaying them on the page

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css';
 function App() {
   return (
     <>
-      <h2 className="title">Tasks</h2>
       <div className="task-container">
+        <h2 className="title">Tasks</h2>
         <label htmlFor="task" className="task-input-label">
           Enter your task:
         </label>
@@ -15,6 +15,40 @@ function App() {
           id="task"
         />
         <button className="btn">Add Task</button>
+      </div>
+
+      <div className="task-list-container">
+        <p className="tasks-completed">Tasks Completed: 2</p>
+        <ul className="task-list">
+          <li className="task">
+            <p>Wash the Dishes</p>
+            <input
+              type="checkbox"
+              name="wash-the-dishes"
+              id="task-checkbox"
+              className="task-checkbox"
+            />
+          </li>
+          <li className="task">
+            <p>Clean the Room</p>
+            <input
+              type="checkbox"
+              name="clean-the-room"
+              id="task-checkbox"
+              className="task-checkbox"
+              checked={true}
+            />
+          </li>
+          <li className="task">
+            <p>Eat Dinner</p>
+            <input
+              type="checkbox"
+              name="eat-dinner"
+              id="task-checkbox"
+              className="task-checkbox"
+            />
+          </li>
+        </ul>
       </div>
     </>
   );
